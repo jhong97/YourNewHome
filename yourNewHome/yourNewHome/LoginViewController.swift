012cf7a9b35ceb50d3d57d97ae0fa7576fc7d94a
Tiffany Chien <tiffany@tiffanys-mbp.lan>
2021-12-03 17:02:56 -0500

    user sign up page and some code

--- yourNewHome/yourNewHome/LoginViewController.swift
+++ yourNewHome/yourNewHome/LoginViewController.swift
@@ -6,16 +6,52 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    //TODO: Link signin
+    @IBAction func onSignIn(_ sender: Any) {
+        print("sign in clicked")
+//      validate username
+        guard let email = emailField.text, !email.isEmpty, let password = passwordField.text, !password.isEmpty else {
+            print("error, missing field data")
+            return
+        }
+        // Get auth instance
+        
+        // attempt sign in
+        
+        // if failure, present alert to create account
+        
+        // if user continues, create account
+                
+        // check sign in on app launch
+        // allow user to sign out with button
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password,  completion: {result, error in guard error == nil else {
+//           TODO: show message that account doesnt exist
+            print("account doesn't exist")
+            return
+        }})
+        print("You have signed in");
+        
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        print("sign up clicked")
+    }
     /*
     // MARK: - Navigation
 

75864ceb03741390142828fe9091f803917bb9dc
John Hong <hongjohn@seas.upenn.edu>
2021-12-10 22:15:37 -0500

    finished login

--- yourNewHome/yourNewHome/LoginViewController.swift
+++ yourNewHome/yourNewHome/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,17 +38,47 @@ class LoginViewController: UIViewController {
         // check sign in on app launch
         // allow user to sign out with button
         
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password,  completion: {result, error in guard error == nil else {
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password,  completion: { [weak self] result, error in guard let strongSelf = self else{
+            return
+        }
+            guard error == nil else {
 //           TODO: show message that account doesnt exist
             print("account doesn't exist")
+                strongSelf.showCreateAccount(email: email, password: password)
             return
-        }})
+        }
+            self?.performSegue(withIdentifier: "loginSegue", sender: nil)
+        })
         print("You have signed in");
+    }
+    
+    func showCreateAccount(email: String, password: String){
+        let alert = UIAlertController(title: "Create Account", message: "Would you like to create an account", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: {[weak self] result, error in guard let strongSelf = self else{
+                return
+            }
+                guard error == nil else{
+                    print("Account creation failed")
+                    
+                    let failedAlert = UIAlertController(title: "Failed", message: "Account creation failed. Please try again", preferredStyle: .alert)
+                    failedAlert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in}))
+                    self?.present(failedAlert, animated: true)
+                    return
+                }
+                
+                print("You have signed in")
+                self?.performSegue(withIdentifier: "loginSegue", sender: nil)
+            })
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {_ in
+        }))
         
-        
+        present(alert, animated: true)
     }
     
     @IBAction func onSignUp(_ sender: Any) {
+        
         print("sign up clicked")
     }
     /*
--- yourNewHome/yourNewHome/SignUpViewController.swift
+++ yourNewHome/yourNewHome/SignUpViewController.swift
@@ -6,16 +6,34 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignUp(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty else{ print ("Email is empty"); return}
+        guard let password = passTextField.text, !password.isEmpty else {print("Password is empty"); return}
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in guard error == nil else {
+            //           TODO: show message that account doesnt exist
+            print(error?.localizedDescription)
+            let failedAlert = UIAlertController(title: "Failed", message: error?.localizedDescription, preferredStyle: .alert)
+            failedAlert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in}))
+            self.present(failedAlert, animated: true)
+            return
+        }
+            print("You have signed up")
+            self.performSegue(withIdentifier: "successSignUp", sender: nil)
+        })
+    }
     /*
     // MARK: - Navigation
 
